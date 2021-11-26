8012340e99b3d97a4118d3d304bae52bce097a1a
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2021-11-26 12:29:13 -0600

    Initial Commit

---
+++ VehicleClass.py
@@ -0,0 +1,13 @@
+class Vehicle:
+    type_of_vehicle = 'Type of Vehicle'
+     maker = 'Make'
+    model = 'Model'
+
+class Cars(Vehicle):
+     body_style = 'Sedan'
+     doors_number = '4'
+
+class Power(Vehicle):
+    engine_type = 'Gas'
+    engine_size = 'V6'
+
