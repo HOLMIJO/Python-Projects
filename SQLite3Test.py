0c9e420c13d6727855d88c15a27df0281a8825d0
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2021-11-29 23:07:28 -0600

    Updated syntax

--- SQLite3Test.py
+++ SQLite3Test.py
@@ -1,7 +1,6 @@
 import sqlite3
-connection = sqlite3.connect("C:\\Users\\joeho\\Dropbox\\School\\Python-Projects\\test.db")
 with sqlite3.connect('test.db') as connection:
-    c.connection.cursor()
+    c=connection.cursor()
     c.executescript("""DROP TABLE IF EXISTS People;
                     CREATE TABLE People(FirstName TEXT, LastName TEXT, Age INT);
                     INSERT INTO People VALUES('Ron', 'Obvious', '42');
