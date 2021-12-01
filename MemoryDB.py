36edbb86142b2463135a28d8045fdef3384082bc
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2021-12-01 11:44:44 -0600

    Updated code

--- MemoryDB.py
+++ MemoryDB.py
@@ -17,14 +17,19 @@ with sqlite3.connect('Roster.db') as conn:
                 cur = conn.cursor()
                 cur.execute("INSERT INTO tbl_roster(Name, Species, IQ) VALUES (?,?,?)", \
                             ('Jean-Baptiste Zorg', 'Human', '122'))
+                conn.commit()
     with conn:
                 cur = conn.cursor()
                 cur.execute("INSERT INTO tbl_roster(Name, Species, IQ) VALUES (?,?,?)", \
                             ('Korben Dallas', 'Meat Popsicle', '100'))
+                conn.commit()
     with conn:
                 cur = conn.cursor()
                 cur.execute("INSERT INTO tbl_roster(Name, Species, IQ) VALUES (?,?,?)", \
                             ('Ak''not', 'Mangalore', '-5'))
+                conn.commit()
+
+
                 cur = conn.cursor()
                 cur.execute("UPDATE roster SET Species = 'Human' WHERE IQ = '100'")
                 cur.execute("SELECT Name, IQ FROM roster WHERE Species = 'Human'")
@@ -38,14 +43,13 @@ with sqlite3.connect('Roster.db') as conn:
                         break
 
 
-
-    """c = connection.cursor()
-    c.execute("DROP TABLE IF EXISTS roster")
-    c.execute("CREATE TABLE roster(Name TEXT, Species TEXT, IQ INT)")
-    c.executemany("INSERT INTO roster VALUES(?,?,?)", rosterValues)
-    c.execute("UPDATE roster SET Species = ? WHERE IQ = 100", 'Human')
-    c.execute("SELECT Name, IQ FROM roster WHERE Species = Human")
-    for row in c.fetchall():
+    #cur = connection.cursor()
+    #cur.execute("DROP TABLE IF EXISTS roster")
+    #cur.execute("CREATE TABLE roster(Name TEXT, Species TEXT, IQ INT)")
+    #cur.executemany("INSERT INTO roster VALUES(?,?,?)", rosterValues)
+    #cur.execute("UPDATE roster SET Species = ? WHERE IQ = 100", 'Human')
+    #cur.execute("SELECT Name, IQ FROM roster WHERE Species = Human")
+    #for row in cur.fetchall():
  
 # cur = conn.cursor()
 # cur.execute(sql)
@@ -53,10 +57,9 @@ with sqlite3.connect('Roster.db') as conn:
 
 # SQLite_connection()
 
-# c.execute("SELECT Name, IQ FROM roster WHERE Species = Human")
+# cur.execute("SELECT Name, IQ FROM roster WHERE Species = Human")
 # while True:
-#     row = c.fetchone()
+#     row = cur.fetchone()
 #     if row is None:
 #             break
 #     print (row)
-"""
