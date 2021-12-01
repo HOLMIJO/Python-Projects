75e0c70c0d2979439fcaf0d1a62fe74f0205d711
HOLMIJO <91238285+HOLMIJO@users.noreply.github.com>
2021-12-01 12:21:21 -0600

    Updated code

--- MemoryDB.py
+++ MemoryDB.py
@@ -1,62 +1,33 @@
 import sqlite3
 
-rosterValues = (('Jean-Baptiste Zorg', 'Human', 122), ('Korben Dallas', 'Meat Popsicle', 100), ('Ak''not', 'Mangalore', -5))
-        
+conn = sqlite3.connect('Roster.db')        
 #execute insert statement for supplied person data
-with sqlite3.connect('Roster.db') as conn:
-    with conn:
-                cur = conn.cursor()
-                cur.execute("CREATE TABLE IF NOT EXISTS tbl_roster( \
-                    ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-                    Name TEXT, \
-                    Species TEXT, \
-                    IQ INT)")
-                conn.commit()
 
-    with conn:
-                cur = conn.cursor()
-                cur.execute("INSERT INTO tbl_roster(Name, Species, IQ) VALUES (?,?,?)", \
-                            ('Jean-Baptiste Zorg', 'Human', '122'))
-    with conn:
-                cur = conn.cursor()
-                cur.execute("INSERT INTO tbl_roster(Name, Species, IQ) VALUES (?,?,?)", \
-                            ('Korben Dallas', 'Meat Popsicle', '100'))
-    with conn:
-                cur = conn.cursor()
-                cur.execute("INSERT INTO tbl_roster(Name, Species, IQ) VALUES (?,?,?)", \
-                            ('Ak''not', 'Mangalore', '-5'))
-                cur = conn.cursor()
-                cur.execute("UPDATE roster SET Species = 'Human' WHERE IQ = '100'")
-                cur.execute("SELECT Name, IQ FROM roster WHERE Species = 'Human'")
-                for row in cur.fetchall():
-                    print(row)
+with conn:
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS tbl_roster( \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
+        Name TEXT, \
+        Species TEXT, \
+        IQ INT)")
+    conn.commit()
 
-                cur.execute("SELECT Name, IQ FROM roster WHERE Species = 'Human'")
-                while True:
-                    row = cur.fetchone()
-                    if row is None:
-                        break
+with conn:
+    cur = conn.cursor()
+    cur.execute("INSERT INTO tbl_roster(Name, Species, IQ) VALUES (?,?,?)", \
+                ('Jean-Baptiste Zorg', 'Human', '122'))
+                
+    cur.execute("INSERT INTO tbl_roster(Name, Species, IQ) VALUES (?,?,?)", \
+                ('Korben Dallas', 'Meat Popsicle', '100'))
 
+    cur.execute("INSERT INTO tbl_roster(Name, Species, IQ) VALUES (?,?,?)", \
+                ('Ak''not', 'Mangalore', '-5'))
+    conn.commit()
 
+with conn:
+    cur = conn.cursor()
+    cur.execute("UPDATE roster SET Species = 'Human' WHERE IQ = '100'")
+    cur.execute("SELECT Name, IQ FROM roster WHERE Species = 'Human'")
+    conn.commit()
 
-    """c = connection.cursor()
-    c.execute("DROP TABLE IF EXISTS roster")
-    c.execute("CREATE TABLE roster(Name TEXT, Species TEXT, IQ INT)")
-    c.executemany("INSERT INTO roster VALUES(?,?,?)", rosterValues)
-    c.execute("UPDATE roster SET Species = ? WHERE IQ = 100", 'Human')
-    c.execute("SELECT Name, IQ FROM roster WHERE Species = Human")
-    for row in c.fetchall():
- 
-# cur = conn.cursor()
-# cur.execute(sql)
-# conn.commit()
-
-# SQLite_connection()
-
-# c.execute("SELECT Name, IQ FROM roster WHERE Species = Human")
-# while True:
-#     row = c.fetchone()
-#     if row is None:
-#             break
-#     print (row)
-"""
+conn.close()
