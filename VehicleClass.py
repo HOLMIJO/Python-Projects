e2fb5ebedeaa274941791f0d31c00d9371951a1d
Joseph Holmin <91238285+HOLMIJO@users.noreply.github.com>
2021-12-15 19:28:36 -0600

    Update VehicleClass.py

--- VehicleClass.py
+++ VehicleClass.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class Vehicle:
     type_of_vehicle = 'Type of Vehicle'
     maker = 'Make'
@@ -11,18 +10,3 @@ class Cars(Vehicle):
 class Power(Vehicle):
     engine_type = 'Gas'
     engine_size = 'V6'
-=======
-class Vehicle:
-    type_of_vehicle = 'Type of Vehicle'
-     maker = 'Make'
-    model = 'Model'
-
-class Cars(Vehicle):
-     body_style = 'Sedan'
-     doors_number = '4'
-
-class Power(Vehicle):
-    engine_type = 'Gas'
-    engine_size = 'V6'
-
->>>>>>> aed72f5c282db2dc5b27b44a61b5f5f9c2fd569c
